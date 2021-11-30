e1f58ea75307db373754e2eb02de48e560723ca0
KuhnVale <valentin.kuhn@utt.fr>
2021-11-30 19:38:05 +0100

    Ajout des premières base de la POO

--- poo.py
+++ poo.py
@@ -1,12 +1,68 @@
 import uuid
 
-class Voter:
-  def __init__(self, lname, fname, email, uuid):
+##--------------------------UTILISATEURS-----------------------------##
+class User:
+  def __init__(self, lname, fname, email):
     self.lname = lname
     self.fname = fname
     self.email = email
-    self.uuid = uuid
 
-v1 = Voter("Moisset", "Oscar", "oscar.moisset@utt.fr", str(uuid.uuid4()))
+  def print(self):
+    print("Utilisateur:", self.fname, self.lname, self.email)
+##-------------------------------------------------------------------##
+class Voter(User):
+  def __init__(self, *args):
+    ### Création d'un électeur depuis un utilisateur
+    if type(args[0]) is User:
+      self.__dict__ = args[0].__dict__.copy()
+    else:
+      super(Voter, self).__init__(*args[:2])
+    self.uuid = uuid.uuid4()
 
-print(v1.email)
+  def from_user(self, user):
+    return self(user.lname, user.fname, user.email)
+
+  def print(self):
+    print("Électeur:", self.fname, self.lname, self.email, self.uuid)
+##-------------------------------------------------------------------##
+class Admin(User):
+  def __init__(self, lname, fname, email):
+    User.__init__(self, lname, fname, email)
+
+  def from_user(self, user):
+    return self(user.fname, user.lname, user.email)
+
+  def print(self):
+    print("Administrateur:", self.fname, self.lname, self.email, self.uuid)
+##-------------------------------------------------------------------##
+class Authority(User):
+  def __init__(self, lname, fname, email):
+    User.__init__(self, lname, fname, email)
+
+  def from_user(self, user):
+    return self(user.fname, user.lname, user.email)
+
+  def print(self):
+    print("Autorité:", self.fname, self.lname, self.email, self.uuid)
+
+##--------------------------ELECTION---------------------------------##
+class Election:
+  class_counter = 0 #Compteur d'élection pour en gérer plusieurs
+  def __init__(self, questions, answers, authorities, voters):
+    self.id = self.class_counter
+    self.questions = questions
+    self.answers = answers
+    self.authorities = authorities
+    self.voters = voters
+    self.class_counter += 1
+
+
+
+
+
+#TESTS
+user = User("Moisset", "Oscar", "oscar.moisset@utt.fr")
+v2 = Voter(user)
+
+user.print()
+v2.print()
