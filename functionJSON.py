318120abfc03557a5a7ce73bd6c8da1230e98230
KuhnVale <valentin.kuhn@utt.fr>
2022-01-08 20:30:46 +0100

    Revert "Modifications majeures :"
    This reverts commit 31dbd84ec74af44fad2991452e37844f853a50e0.

--- functionJSON.py
+++ functionJSON.py
@@ -1,6 +1,4 @@
 import json
-import struct
-
 
 ###---------- FONCTIONS JSON----------###
 
@@ -22,32 +20,12 @@ def deleteAllVote():
     with open("json/votes.json", "w") as votes:
         json.dump(data, votes)
 
-
-def deleteVote(voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        dataVote.remove(matchVote)
-
-    with open("json/votes.json", "w") as votes:
-        json.dump(dataVote, votes)
-
-
-def getAllVotes():
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        return dataVote
-
-
 def addVote(vote):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
         dataVote.append(vote)
     with open("json/votes.json", "w") as votes:
         json.dump(dataVote, votes)
-
 
 def getVote(voteID):
     with open("json/votes.json", "r") as votes:
@@ -59,21 +37,17 @@ def getVote(voteID):
             return 0
         return matchVote
 
-
 def addVoter(user, voteID):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
 
         # Verification : L'utilisateur doit avoir un code de vote pour ce vote
-        uuid = getUUID(user, voteID)
-        cDv = getVoteCode(user, voteID)
+        cDv = getCredential(user, voteID)
         matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        matchVote["Voters"].append(uuid)
-        matchVote["VoteCodes"].append(cDv)
+        matchVote["Voters"].append(cDv)
 
     with open("json/votes.json", "w") as votes:
         json.dump(dataVote, votes)
-
 
 def addAdmin(user, voteID):
     with open("json/votes.json", "r") as votes:
@@ -87,32 +61,37 @@ def addAdmin(user, voteID):
     with open("json/votes.json", "w") as votes:
         json.dump(dataVote, votes)
 
-
 def getAdmin(voteID):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
         return next((vote["Admin"] for vote in dataVote if vote["ID"] == voteID), 0)
-
 
 def addAuthorized(user, voteID):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
 
         # Verification : L'utilisateur doit avoir un UUID pour ce vote
-        uuidUser = getUUID(user, voteID)
+        matchUser = next((pair for pair in user["uuids"] if pair["voteID"] == voteID), 0)
+        if not matchUser:
+            print("Erreur lors de l'ajout de l'autorisée : l'utilisateur n'a pas d'UUID pour ce vote.")
+            return
         matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
         if not matchVote:
-            print("Erreur lors de l'ajout de l'autorisée")
+            print("Erreur lors de l'ajout de l'autorisée : le vote n'a pas été trouvé.")
             return
-        matchVote["Authorized"].append(uuidUser)
+        matchVote["Authorized"].append(matchUser["uuid"])
 
     with open("json/votes.json", "w") as votes:
         json.dump(dataVote, votes)
 
-
 def findVotesWhereVoter(user):
-    return [getVote(credential["voteID"]) for credential in user["Credentials"]]
+    with open("json/votes.json", "r") as votes:
+        dataVote = json.load(votes)
 
+        return [vote for vote in dataVote
+                for person in user["uuids"]
+                for person["uuid"] in vote["Voters"]
+                ]
 
 def findVotesWhereAdmin(user):
     with open("json/votes.json", "r") as votes:
@@ -123,7 +102,6 @@ def findVotesWhereAdmin(user):
                 if vote["Admin"] == item["uuid"]
                 ]
 
-
 def findVotesWhereAuthorized(user):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
@@ -133,81 +111,16 @@ def findVotesWhereAuthorized(user):
                 for person["uuid"] in vote["Authorized"]
                 ]
 
-
-def emptyVoteVoteCodes(voteID):
+def verifyVoteCode(cDv, voteID):
     with open("json/votes.json", "r") as votes:
         dataVote = json.load(votes)
 
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        matchVote["VoteCodes"] = []
-
-    with open("json/votes.json", "w") as votes:
-        json.dump(dataVote, votes)
-
-
-def addVoteVoteCode(voteID, cDv):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        matchVote["VoteCodes"].append(cDv)
-
-    with open("json/votes.json", "w") as votes:
-        json.dump(dataVote, votes)
-
-
-def verifyVoteCode(code, voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-    matchVote = getVote(voteID)
-    if not matchVote:
-        return False
-    if code in matchVote["VoteCodes"]:
-        return True
-    return False
-
-
-def addBallot(ballot, voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        matchVote["Ballots"].append(ballot)
-
-    with open("json/votes.json", "w") as votes:
-        json.dump(dataVote, votes)
-
-
-def getAllBallots(voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        return matchVote["Ballots"]
-
-
-def getAllVoteCodes(voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        return matchVote["VoteCodes"]
-
-
-def setVoteResult(winner, total, voteID):
-    with open("json/votes.json", "r") as votes:
-        dataVote = json.load(votes)
-
-        matchVote = next((vote for vote in dataVote if vote["ID"] == voteID), 0)
-        data = {
-            "Response": winner,  # String
-            "Count": total  # Int
-        }
-        matchVote["Result"] = data
-
-    with open("json/votes.json", "w") as votes:
-        json.dump(dataVote, votes)
+        matchVote = next((vote["Admin"] for vote in dataVote if vote["ID"] == voteID), 0)
+        if not matchVote:
+            return 0
+        if cDv in matchVote["Voters"]:
+            return 1
+        return 0
 
 
 ##---- USERS.JSON ----##
@@ -216,25 +129,17 @@ def getUser(lname, fname, email):
         dataUser = json.load(users)
 
         return next((user for user in dataUser
-                     if user["lname"].lower() == lname.lower()
-                     and user["fname"].lower() == fname.lower()
-                     and user["mail"].lower() == email.lower()), [])
-
-
-def getAllUsers():
-    with open("./json/users.json") as users:
-        dataUser = json.load(users)
-        return dataUser
-
+                          if user["lname"].lower() == lname.lower()
+                          and user["fname"].lower() == fname.lower()
+                          and user["mail"].lower() == email.lower()), 0)
 
 def getUserFromUUID(uuid):
     with open("./json/users.json") as users:
         dataUser = json.load(users)
 
         return next((user for user in dataUser
-                     for item in user["uuids"]
-                     if user["uuid"] == uuid), 0)
-
+                          for item in user["uuids"]
+                          if user["uuid"] == uuid), 0)
 
 def addUser(user):
     with open("./json/users.json") as users:
@@ -243,7 +148,6 @@ def addUser(user):
 
     with open("./json/users.json", "w") as users:
         json.dump(dataUser, users)
-
 
 def addUUID(user, voteID, uuid):
     with open("./json/users.json", "r") as users:
@@ -263,16 +167,15 @@ def addUUID(user, voteID, uuid):
     with open("./json/users.json", "w") as users:
         json.dump(data, users)
 
-
 def getUUID(user, voteID):
     with open("./json/users.json", "r") as users:
         data = json.load(users)
 
         matchUser = next((person for person in data
-                          if person["lname"].lower() == user["lname"].lower()
-                          and person["fname"].lower() == user["fname"].lower()
-                          and person["mail"].lower() == user["mail"].lower())
-                         , 0)
+                      if person["lname"].lower() == user["lname"].lower()
+                      and person["fname"].lower() == user["fname"].lower()
+                      and person["mail"].lower() == user["mail"].lower())
+                     , 0)
 
         if not matchUser:
             print("Erreur lors de la récupération de l'UUID : l'utilisateur n'a pas de compte.")
@@ -280,32 +183,31 @@ def getUUID(user, voteID):
 
         matchVote = next((uuid for uuid in matchUser["uuids"]
                           if uuid["voteID"] == voteID)
-                         , 0)
+                         ,0)
         if not matchUser:
             print("Erreur lors de la récupération de l'UUID : l'utilisateur n'a pas accès au vote {}.".format(voteID))
             return
 
         return matchVote["uuid"]
 
-
-def setUserVoteCode(user, voteID, cDv):
+def addVoteCode(user, voteID, cDv):
     with open("./json/users.json", "r") as users:
         data = json.load(users)
 
-        matchUser = next((person for person in data
-                          if person["lname"].lower() == user["lname"].lower()
-                          and person["fname"].lower() == user["fname"].lower()
-                          and person["mail"].lower() == user["mail"].lower())
-                         , 0)
+        match = next((person for person in data
+                      if person["lname"].lower() == user["lname"].lower()
+                      and person["fname"].lower() == user["fname"].lower()
+                      and person["mail"].lower() == user["mail"].lower())
+                     , 0)
 
-        matchingVote = next((infos for infos in matchUser["Credentials"]
-                             if infos["voteID"] == voteID), 0)
+        if not match:
+            print("Erreur lors de l'ajout du code de vote : l'utilisateur n'a pas de compte.")
+            return
 
-        matchingVote["voteCode"] = cDv
+        match["Credentials"].append({"voteID": voteID, "voteCode": cDv})
 
     with open("./json/users.json", "w") as users:
         json.dump(data, users)
-
 
 def getVoteCode(user, voteID):
     with open("./json/users.json", "r") as users:
@@ -331,8 +233,7 @@ def getVoteCode(user, voteID):
 
         return matchCode["voteCode"]
 
-
-def addCredentials(user, voteID, credential, cDv):
+def addCredential(user, voteID, credential):
     with open("./json/users.json", "r") as users:
         data = json.load(users)
 
@@ -346,21 +247,20 @@ def addCredentials(user, voteID, credential, cDv):
             print("Erreur lors de l'ajout du credential' : l'utilisateur n'a pas de compte.")
             return
 
-        match["Credentials"].append({"voteID": voteID, "credential": credential, "voteCode": cDv})
+        match["Credentials"].append({"voteID": voteID, "credential": credential})
 
     with open("./json/users.json", "w") as users:
         json.dump(data, users)
-
 
 def getCredential(user, voteID):
     with open("./json/users.json", "r") as users:
         data = json.load(users)
 
         matchUser = next((person for person in data
-                          if person["lname"].lower() == user["lname"].lower()
-                          and person["fname"].lower() == user["fname"].lower()
-                          and person["mail"].lower() == user["mail"].lower())
-                         , 0)
+                      if person["lname"].lower() == user["lname"].lower()
+                      and person["fname"].lower() == user["fname"].lower()
+                      and person["mail"].lower() == user["mail"].lower())
+                     , 0)
 
         if not matchUser:
             print("Erreur lors de la récupération du credential : l'utilisateur n'a pas de compte.")
@@ -368,10 +268,38 @@ def getCredential(user, voteID):
 
         matchCode = next((credential for credential in matchUser["Credentials"]
                           if credential["voteID"] == voteID)
-                         , 0)
+                         ,0)
         if not matchUser:
-            print(
-                "Erreur lors de la récupération du credential : l'utilisateur n'a pas accès au vote {}.".format(voteID))
+            print("Erreur lors de la récupération du credential : l'utilisateur n'a pas accès au vote {}.".format(voteID))
             return
 
         return matchCode["credential"]
+
+# def addMDP(user, voteID, mdp):
+#     with open("./json/users.json", "r") as users:
+#         data = json.load(users)
+#
+#         match = next((person for person in data
+#                       if person["lname"].lower() == user["lname"].lower()
+#                       and person["fname"].lower() == user["fname"].lower()
+#                       and person["mail"].lower() == user["mail"].lower())
+#                      , 0)
+#
+#         if not match:
+#             print("Erreur lors de la génération du mot de passe : l'utilisateur n'a pas de compte.")
+#             return
+#
+#
+#         match["Passwords"].append({"voteID": voteID, "mdp": mdp})
+#
+# def verifyPassword(mdp, voteID):
+#     with open("json/users.json", "r") as users:
+#         dataUser = json.load(users)
+#
+#         matchUser = next((user for user in dataUser
+#                           for pw in user["Passwords"]
+#                           if pw["voteID"] == voteID
+#                           and pw["mdp"] == mdp), 0)
+#         if not matchUser:
+#             return 0
+#         return 1
